de2262b1638a0620c74ae1c93dac26bc20b7666d
Jason Wu <jwu047@ucr.edu>
2019-11-17 01:35:46 -0800

    repetition fix
    added boolean conditional for test

--- jw_07_plotbot.py
+++ jw_07_plotbot.py
@@ -50,65 +50,68 @@ def AnalyzeTweets():
     if all_plotbot_tweets == list():
         api.update_status("Excited to start tweeting!")
         all_plotbot_tweets = api.user_timeline()
-        
+
+    repeat=False 
     # Required that account has a minimum of one tweet
     for tweet in all_plotbot_tweets:
         # Check if repeat
         if target_account in tweet["text"]:
+            repeat=True
             print("Repeated request.")
             # If repeat, stop here
             return
         else:
-            print("Request in progress...")
-            # make sure to keep outside for loop
-            tweet_data = {
-                "tweet_source": [],
-                "tweet_text": [],
-                "tweet_date": [],
-                "tweet_vader_score": [],
-                "tweet_neg_score": [],
-                "tweet_pos_score": [],
-                "tweet_neu_score": []
-            }
-            # Paginate for 500 tweets
-            for x in range(25):
-                public_tweets = api.user_timeline(target_account, page=x)
-                for tweet in public_tweets:
-                    tweet_data["tweet_source"].append(tweet["user"]["name"])
-                    tweet_data["tweet_text"].append(tweet["text"])
-                    tweet_data["tweet_date"].append(tweet["created_at"])
+            continue
+    if not (repeat):
+        print("Request in progress...")
+        # make sure to keep outside for loop
+        tweet_data = {
+            "tweet_source": [],
+            "tweet_text": [],
+            "tweet_date": [],
+            "tweet_vader_score": [],
+            "tweet_neg_score": [],
+            "tweet_pos_score": [],
+            "tweet_neu_score": []
+        }
+        # Paginate for 500 tweets
+        for x in range(25):
+            public_tweets = api.user_timeline(target_account, page=x)
+            for tweet in public_tweets:
+                tweet_data["tweet_source"].append(tweet["user"]["name"])
+                tweet_data["tweet_text"].append(tweet["text"])
+                tweet_data["tweet_date"].append(tweet["created_at"])
 
-                    results = analyzer.polarity_scores(tweet["text"])
-                    tweet_data["tweet_vader_score"].append(results["compound"])
-                    tweet_data["tweet_pos_score"].append(results["pos"])
-                    tweet_data["tweet_neu_score"].append(results["neu"])
-                    tweet_data["tweet_neg_score"].append(results["neg"])
-            tweet_df = pd.DataFrame.from_dict(tweet_data)
-            # Convert to datetime objects then sort
-            tweet_df["tweet_date"] = pd.to_datetime(tweet_df["tweet_date"])
-            tweet_df.sort_values("tweet_date", inplace=True)
-            tweet_df.reset_index(drop=True, inplace=True)
-            
-            # Clear plot
-            plt.clf()
-            plt.plot(np.arange(-len(tweet_df["tweet_vader_score"]), 0, 1),
-                         tweet_df["tweet_vader_score"], marker="o", linewidth=0.5,
-                         alpha=0.8, label=f"{target_account}")
-            plt.title(f"Sentiment Analysis of Tweets ({time.strftime('%x')}")
-            plt.ylabel("Tweet Polarity")
-            plt.xlabel("Tweets Ago")
-            plt.xlim([-len(tweet_df["tweet_vader_score"]) - 7, 7])
-            plt.ylim([-1.05, 1.05])
-            plt.grid()
+                results = analyzer.polarity_scores(tweet["text"])
+                tweet_data["tweet_vader_score"].append(results["compound"])
+                tweet_data["tweet_pos_score"].append(results["pos"])
+                tweet_data["tweet_neu_score"].append(results["neu"])
+                tweet_data["tweet_neg_score"].append(results["neg"])
+        tweet_df = pd.DataFrame.from_dict(tweet_data)
+        # Convert to datetime objects then sort
+        tweet_df["tweet_date"] = pd.to_datetime(tweet_df["tweet_date"])
+        tweet_df.sort_values("tweet_date", inplace=True)
+        tweet_df.reset_index(drop=True, inplace=True)
+        
+        # Clear plot
+        plt.clf()
+        plt.plot(np.arange(-len(tweet_df["tweet_vader_score"]), 0, 1),
+                        tweet_df["tweet_vader_score"], marker="o", linewidth=0.5,
+                        alpha=0.8, label=f"{target_account}")
+        plt.title(f"Sentiment Analysis of Tweets ({time.strftime('%x')}")
+        plt.ylabel("Tweet Polarity")
+        plt.xlabel("Tweets Ago")
+        plt.xlim([-len(tweet_df["tweet_vader_score"]) - 7, 7])
+        plt.ylim([-1.05, 1.05])
+        plt.grid()
 
-            lgnd = plt.legend(fontsize="small", mode="Expanded",
-                                      loc="best", bbox_to_anchor=(1, 1), title="Tweets", labelspacing=0.5)
-            file_path = "analysis/" + target_account + ".png"
-            plt.savefig(file_path, bbox_extra_artists=(lgnd, ), bbox_inches='tight')
-            print(f"Plot complete for {target_account}!")
-            api.update_with_media(file_path, f"New Tweet Analysis: {target_account} (Thx @{username}!)")
-            time.sleep(15)
-            print(f"Updated status for {target_account}!")
+        lgnd = plt.legend(fontsize="small", mode="Expanded",
+                                    loc="best", bbox_to_anchor=(1, 1), title="Tweets", labelspacing=0.5)
+        file_path = "analysis/" + target_account + ".png"
+        plt.savefig(file_path, bbox_extra_artists=(lgnd, ), bbox_inches='tight')
+        print(f"Plot complete for {target_account}!")
+        api.update_with_media(file_path, f"New Tweet Analysis: {target_account} (Thx @{username}!)")
+        print(f"Updated status for {target_account}!")
             
 
 while(True):
